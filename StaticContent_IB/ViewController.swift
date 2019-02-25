293caadfdd0866329bd187d30c65a99a711d9e76
Ian Campbell Brothers <iab15a@lib-301-09-9118.local>
2019-02-25 08:18:53 -0600

    Initial Commit

---
+++ StaticContent_IB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaticContent_IB
+//
+//  Created by Ian Campbell Brothers on 2/25/19.
+//  Copyright © 2019 Ian Campbell Brothers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
